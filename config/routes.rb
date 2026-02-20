834e05d94982b0f3b371fd98f740ebbdd2cd8596
Katherine Rush <108463876+krushy8@users.noreply.github.com>
2026-02-20 10:42:43 +0900

    fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "lists#index"
+  root to: "lists#index"
   resources :lists do
     resources :bookmarks, only: [ :new, :create, :destroy ]
   end
